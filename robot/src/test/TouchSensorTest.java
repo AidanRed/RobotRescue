23255f9282de16c6ed5ed5109f2eac7f2e24d75a
Abdul Mohsi Jawaid <abdulmohsi.jawaid@adelaide.edu.au>
2018-08-24 16:33:25 +0930

    Changed code to work wirelessly

--- robot/src/test/TouchSensorTest.java
+++ robot/src/test/TouchSensorTest.java
@@ -4,6 +4,13 @@ Testing Touch/Bump Sensor
 Displays 1 or 0 depending on sensor reading until quit
 
 */
+
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+import lejos.remote.ev3.RemoteEV3;
+
 import lejos.hardware.Brick;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.sensor.EV3TouchSensor;
@@ -13,23 +20,25 @@ import lejos.hardware.port.Port;
 import lejos.robotics.SampleProvider;
 
 public class TouchSensorTest{
-        public static void main(String[] args){
-              Brick brick = BrickFinder.getDefault();
-                Port s1 = brick.getPort("S1");
-                EV3TouchSensor sensor = new EV3TouchSensor(s1);
-                SampleProvider provider;
-                float[] sample;
+    public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException {
 
-                provider = sensor.getTouchMode();
-                sample = new float[provider.sampleSize()];
+        RemoteEV3 ev3 = new RemoteEV3("192.168.43.132");
+        
+        Port s1 = ev3.getPort("S1");
+        EV3TouchSensor sensor = new EV3TouchSensor(s1);
+        SampleProvider provider;
+        float[] sample;
 
-                while(true){
-                        provider.fetchSample(sample, 0);
-                        System.out.println(sample[0]);
-                        if(Button.ESCAPE.isDown()){
-                            sensor.close();
-                            System.exit(0);
-                        }
-                } 
-        }
+        provider = sensor.getTouchMode();
+        sample = new float[provider.sampleSize()];
+
+        while(true){
+            provider.fetchSample(sample, 0);
+            System.out.println(sample[0]);
+            if(Button.ESCAPE.isDown()){
+                sensor.close();
+                System.exit(0);
+            }
+        } 
+    }
 }
