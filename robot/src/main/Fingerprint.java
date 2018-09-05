ffbb00d32cc91abc8a800c391834f04a4f062668
Trek Hopton <trek.hopton@gmail.com>
2018-09-05 20:20:10 +0930

    Gathering device data

--- robot/src/main/Fingerprint.java
+++ robot/src/main/Fingerprint.java
@@ -1,10 +1,13 @@
 import java.security.MessageDigest;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.ev3.Port;
+// class for creating and storing a unique digital fingerprint 
+// to identify the robot
 public class Fingerprint {
     private byte[] fingerprint;
-
+    //construct a fingerprint
     public Fingerprint(){
         fingerprint = generateFingerprint();
     }
@@ -12,13 +15,23 @@ public class Fingerprint {
     public byte[] getFingerprint(){
         return fingerprint;
     }
-
+    // find unique device data and concatinate it into one string
     private String findData(){
         //getting robot info
-        String toHash = getDeviceSignature(true);
+        String toHash = "";
+        //add device information for each port
+        String[] portNames = {"S1", "S2", "S3", "S4", "A", "B", "C", "D"};
+        for(int i = 0; i < 8; i++){
+            Port p = LocalEV3.get().getPort(portNames[i]);
+            DeviceIdentifier devId = new DeviceIdentifier(p);
+            toHash += devId.getDeviceSignature(true);
+        }
+        
+        // add more EV3 info here
+
         return toHash;
     }
-
+    // hash robot data into a byte array which is a fingerprint
     private byte[] generateFingerprint(){
         String toHash = findData();
         MessageDigest digest;
@@ -28,10 +41,10 @@ public class Fingerprint {
             digest = null;
         }
         byte[] fp = digest.digest(toHash.getBytes(StandardCharsets.UTF_8));
-        //do hashing here and return
+
         return fp;
     }
-
+    //this is just for testing and will be removed
     public static void main(String[] args){
         Fingerprint myFp = new Fingerprint();
         System.out.println(Arrays.toString(myFp.getFingerprint()));
