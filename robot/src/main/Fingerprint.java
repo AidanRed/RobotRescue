0358a14237cb2f8cb9389524ac44209be77c8aeb
Sivadius <davismartinjohn@gmail.com>
2018-10-08 14:50:22 +1030

    Fingerprinting test works now

--- robot/src/main/Fingerprint.java
+++ robot/src/main/Fingerprint.java
@@ -5,6 +5,8 @@ import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.port.Port;
 import lejos.hardware.device.DeviceIdentifier;
 import java.util.Scanner;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.port.MotorPort;
 
 // class for creating and storing a unique digital fingerprint 
 // to identify the robot
@@ -23,10 +25,12 @@ public class Fingerprint {
         //getting robot info
         String toHash = "";
         //add device information for each port
-        String[] portNames = {"S1", "S2", "S3", "S4", "A", "B", "C", "D"};
-        for(int i = 0; i < 8; i++){
-            Port p = LocalEV3.get().getPort(portNames[i]);
-            DeviceIdentifier devId = new DeviceIdentifier(p);
+//        String[] portNames = {"S1", "S2", "S3", "S4", "A", "B", "C", "D"};
+        Port[] port= {SensorPort.S1, SensorPort.S2, SensorPort.S3, SensorPort.S4, 
+                MotorPort.A, MotorPort.B, MotorPort.C, MotorPort.D};
+        for(int i = 0; i < port.length; i++){
+//            Port p = LocalEV3.get().getPort(portNames[i]);
+            DeviceIdentifier devId = new DeviceIdentifier(port[i]);
             toHash += devId.getDeviceSignature(true);
             devId.close();
         }
@@ -52,22 +56,41 @@ public class Fingerprint {
     public static void main(String[] args){
         Fingerprint myFp = new Fingerprint();
         byte[] first = myFp.getFingerprint();
+//        String first = myFp.findData();
 
         // Wait for an input
-        Scanner reader = new Scanner(System.in);
-        System.out.println("Press 'Enter' to re-generate fingerprint.");
-        reader.nextLine();
-        reader.close();
+//        Scanner reader = new Scanner(System.in);
+        System.out.println("You have aproximately 5 seconds to modify the robot.");
+        try {
+            Thread.sleep(5000);
+        }
+        catch(Exception e) {
+            System.out.println("It messed up");
+        }
+//        reader.nextLine();
+//        reader.close();
         
         // Output results
-        byte[] second = myFp.getFingerprint();
+        Fingerprint myFp2 = new Fingerprint();
+        byte[] second = myFp2.getFingerprint();
         System.out.println("First = " + Arrays.toString(first));
         System.out.println("Second = " + Arrays.toString(second));
+//      String second = myFp.findData();
+//      System.out.println("First = " + first);
+//      System.out.println("Second = " + second);
+        
         // Compare fingerprints
-        if(first != second) {
+//        if(!first.equals(second)) {
+        if(!Arrays.equals(first, second)) {
             System.out.println("Fingerprint has changed!");
         } else {
             System.out.println("Fingerprint is the same!");
+        }
+        try {
+            Thread.sleep(10000);
+        }
+        catch(Exception e) {
+            System.out.println("It messed up");
         }
     }
 }
