0fd58767c77fc933dbcf9400a0db070b52468d9a
Aidan Redding <aidanhred@gmail.com>
2018-10-09 20:23:14 +1030

    Fixed bugs, changed sensors to be remote, improved ColorSensor test and added GyroSensorTest

--- robot/src/test/hardware/ColorSensorTest.java
+++ robot/src/test/hardware/ColorSensorTest.java
@@ -10,42 +10,59 @@ import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
+import lejos.remote.ev3.RMISampleProvider;
 import lejos.hardware.Button;
-import lejos.hardware.port.Port;
-import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.robotics.SampleProvider;
 import lejos.robotics.Color;
 import lejos.remote.ev3.RemoteEV3;
-import lejos.utility.Delay;
+import lejos.hardware.KeyListener;
+import lejos.hardware.Button;
+import lejos.hardware.Key;
 
 public class ColorSensorTest 
 {
-    public static void main(String[] args) throws RemoteException, MalformedURLException, NotBoundException 
-    {
-        RemoteEV3 ev3 = RobotUtility.findBrick();
-        EV3ColorSensor colorSensor = RobotUtility.getColorSensor(ev3);
-        
-        // Exits test if Down button pressed
-        while(Button.DOWN.isUp())
-        {
-            int currentDetectColor = colorSensor.getColorID();
-            switch(currentDetectColor){
-                case Color.RED:
-                    System.out.println("RED DETECTED");
-                    break;
-                case Color.BLUE:
-                    System.out.println("BLUE DETECTED");
-                    break;
-                case Color.GREEN:
-                    System.out.println("GREEN DETECTED");
-                    break;
-                default:
-                    System.out.println("NONE DETECTED");
-                    break;
+    static boolean running = false;
 
+    static void exit(){
+        running = false;
+        RobotUtility.closeSensors();
+    }
+
+    public static void main(String[] args) throws RemoteException
+    {
+        running = true;
+        RemoteEV3 ev3 = RobotUtility.findBrick();
+        RMISampleProvider colorSensor = RobotUtility.getColorSensor(ev3);
+        int lastColor = (int)colorSensor.fetchSample()[0];
+        
+        // Add a listener to the escape button to exit the program
+        Button.ESCAPE.addKeyListener(new KeyListener() {
+            public void keyPressed(Key k){
+                exit();
             }
-            Delay.msDelay(100);
+            public void keyReleased(Key k){
+            }
+        });
+
+        while(running)
+        {
+            int currentColor = (int)colorSensor.fetchSample()[0];
+            if(currentColor != lastColor){
+                lastColor = currentColor;
+                switch(lastColor){
+                    case Color.RED:
+                        System.out.println("RED DETECTED");
+                        break;
+                    case Color.BLUE:
+                        System.out.println("BLUE DETECTED");
+                        break;
+                    case Color.GREEN:
+                        System.out.println("GREEN DETECTED");
+                        break;
+                    default:
+                        System.out.println("NONE DETECTED");
+                        break;
+                }
+            }
         }
-        ev3.closeSensors();
     }
 }
